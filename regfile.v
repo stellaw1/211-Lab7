9aa282cb70bd799a36de20c54df598eb812900f8
Stella W <46392676+stellaw1@users.noreply.github.com>
2019-10-06 17:28:15 -0700

    wrote regfile.v and regfile_tb.v

--- regfile.v
+++ regfile.v
@@ -4,6 +4,98 @@ module regfile(data_in, writenum, write, readnum, clk, data_out);
     input write, clk;
     output [15:0] data_out;
 
-    
+    //decode writenum from binary to 8-bit one hot
+    wire [7:0] writeNumOneHot;
+    Dec #(3, 8) writeNum(writenum, writeNumOneHot);
+
+    //declare 8 16-bit buses to store data in each register
+    wire [15:0] data_0; 
+    wire [15:0] data_1;
+    wire [15:0] data_2;
+    wire [15:0] data_3;
+    wire [15:0] data_4;
+    wire [15:0] data_5;
+    wire [15:0] data_6;
+    wire [15:0] data_7;
+
+    //instantiate 8 16-bit registers, R0-R7
+    //DEBUG: might need to make a module for writing files
+    vDFFE #(16) R0(clk, writeNumOneHot[0] & write, data_in, data_0); 
+    vDFFE #(16) R1(clk, writeNumOneHot[1] & write, data_in, data_1); 
+    vDFFE #(16) R2(clk, writeNumOneHot[2] & write, data_in, data_2); 
+    vDFFE #(16) R3(clk, writeNumOneHot[3] & write, data_in, data_3); 
+    vDFFE #(16) R4(clk, writeNumOneHot[4] & write, data_in, data_4); 
+    vDFFE #(16) R5(clk, writeNumOneHot[5] & write, data_in, data_5); 
+    vDFFE #(16) R6(clk, writeNumOneHot[6] & write, data_in, data_6); 
+    vDFFE #(16) R7(clk, writeNumOneHot[7] & write, data_in, data_7); 
+
+    //instantiate read file
+    enableRead readRegister(readnum, data_out, data_0, data_1, data_2, data_3, data_4, data_5, data_6, data_7);
+
+endmodule
+
+
+//Source: Lab5 slides 
+//Register with load enable
+//
+// Parameters: 
+// n = width of data
+module vDFFE(clk, en, in, out) ;
+  parameter n = 1; 
+  input clk, en ;
+  input  [n-1:0] in ;
+  output [n-1:0] out ;
+  reg    [n-1:0] out ;
+  wire   [n-1:0] next_out ;
+
+  assign next_out = en ? in : out;
+
+  always @(posedge clk)
+    out = next_out;  
+endmodule
+
+
+//Source: slide set 6
+// n:m decoder
+//
+// Parameters:
+// a - binary input   (n bits wide)
+// b - one hot output (m bits wide)
+module Dec(a, b) ;
+  parameter n=2 ;
+  parameter m=4 ;
+
+  input  [n-1:0] a ;
+  output [m-1:0] b ;
+
+  wire [m-1:0] b = 1 << a ;
+endmodule
+
+
+//to read data from selected register
+module enableRead(readnum, data_out, data_0, data_1, data_2, data_3, data_4, data_5, data_6, data_7);
+    input [2:0] readnum;
+    input [15:0] data_0, data_1, data_2, data_3, data_4, data_5, data_6, data_7;
+    output [15:0] data_out;
+    reg [15:0] data_out; //DEBUG: might need to rename
+
+    //decode readnum from binary to 8-bit one hot
+    wire [7:0] readOneHot;
+    Dec #(3, 8) readNum(readnum, readOneHot);
+
+    //everytime readnum changes, reassign data_out to be data stored in corresponding register
+    always @(readOneHot) begin
+        case (readOneHot)
+            8'b00000001: data_out = data_0;
+            8'b00000010: data_out = data_1;
+            8'b00000100: data_out = data_2;
+            8'b00001000: data_out = data_3;
+            8'b00010000: data_out = data_4;
+            8'b00100000: data_out = data_5;
+            8'b01000000: data_out = data_6;
+            8'b10000000: data_out = data_7;
+            default: data_out = 16'bxxxxxxxxxxxxxxxx;
+        endcase
+    end
 
 endmodule
--- regfile_tb.v
+++ regfile_tb.v
@@ -5,6 +5,71 @@ module regfile_tb();
     reg err;
     wire [15:0] sim_data_out;
 
+    //instantiate DUT: regfile module
+    regfile DUT(sim_data_in, sim_writenum, sim_write, sim_readnum, sim_clk, sim_data_out);
+
     
+    task error_checker;
+        input [15:0] expected_data_out;
+        begin
+            //if the data_out in the instantiated DUT is erraneous, display the actual and expected state, and set err = 1
+            if (DUT.data_out !== expected_data_out) begin
+                $display("ERROR: data_out is %b, expecting %b", DUT.data_out, expected_data_out);
+                err = 1'b1;
+            end
+        end
+    endtask
+
+    //DEBUG: Do I need a forever loop? Can I just manually change clk for every write testcase
+    // //loop of rising edges in clk
+    // initial begin
+    //     //default clk to be 0
+    //     clk = 1'b0; 
+    //     #5;
+
+    //     //loop clk to have a rising edge every 10 time units
+    //     forever begin
+    //         clk = 1'b1; 
+    //         #5;
+    //         clk = 1'b0; 
+    //         #5;
+    //     end
+    // end
+
+    //testcases go here
+    initial begin
+        //initialize signals
+        sim_clk = 1'b0;
+        err = 1'b0;
+        sim_data_in = 16'b1;
+        #5;
+
+        //testcase 1: test writing to register 0, data_in = 16'b1
+        sim_writenum = 3'b0;
+        sim_write = 1'b1;
+        #5;
+
+        sim_clk = 1;
+        #5;
+
+        $display("data_0 is %b, expecting %b", DUT.data_0, 16'b1);
+
+        //testcase 2: read from register 0
+        sim_readnum = 3'b0;
+        #5;
+
+        error_checker(16'b1);
+        $display("data_out is %b, expecting %b", sim_data_out, 16'b1);
+
+        
+        //print results, whether errors were found
+        if (~err) 
+            $display ("All test cases passed with no errors. ");
+        else
+            $display ("Error found. ");
+
+        //stop forever loop of clk rising edges
+        $stop;
+    end
 
 endmodule
