3e252ec1731bcbb7cf657b4bc904e7fef163b618
baknelmacz <baknelmacz@users.noreply.github.com>
2019-10-06 18:48:37 -0700

    added ALU and ALU_tb

--- alu.v
+++ alu.v
@@ -1,8 +1,69 @@
 module ALU(Ain, Bin, ALUop, out, Z);
-    input [15:0] Ain, Bin;
-    input [1:0] ALUop;
-    output [15:0] out;
-    output Z;
+  input [15:0] Ain, Bin;
+  input [1:0] ALUop;
+  output [15:0] out;
+  output Z;
 
+  wire [15:0] add_val, sub_val, and_val, not_b;
+  
+  //calculate possible output values
+  assign add_val = Ain + Bin;
+  assign sub_val = Ain - Bin;
+  assign and_val = Ain & Bin;
+  assign not_b = ~Bin;
 
+  
+  //MUX that selects which operation to output 
+  Mux_k_4_binary #(16) alu_MUX(not_b,and_val,sub_val,add_val, ALUop, out);
+  assign Z = ~(|out); //assign Z = 1 if output is all 0's
+endmodule
+
+//k bit ,4 input, binary select MUX
+module Mux_k_4_binary(a3, a2, a1, a0, sb, b);
+  parameter k = 1 ;
+  input [k-1:0] a3, a2, a1, a0; //inputs
+  input [1:0] sb; //select
+  output [k-1:0] b;
+  wire [3:0] s;
+
+  Dec #(2,4) de(sb, s); //decoder takes binary input and gives one hot output
+  Mux4 #(k) m(a3, a2, a1, a0, s, b); 
+endmodule
+  
+//4 input k bit MUX, one hot select
+module Mux4(a3, a2, a1, a0, s, b);
+  parameter k = 1;
+  input [k-1:0] a3, a2, a1, a0; //inputs
+  input [3:0] s; //select
+  output [k-1:0] b;
+  wire [k-1:0] b =   ({k{s[0]}} & a0) |
+		     ({k{s[1]}} & a1) |
+		     ({k{s[2]}} & a2) |
+		     ({k{s[3]}} & a3) ;
+endmodule
+
+//k bit selecter MUX (used in data path)
+module Mux2(a1, a0, s, b);
+  parameter k = 1;
+  input [k-1:0] a1, a0; //inputs
+  input s; //select
+  output [k-1:0] b;
+  wire [k-1:0] b =   ({k{~s}} & a0) |
+		     ({k{s}} & a1) ;
+endmodule
+
+//Source: slide set 6
+// n:m decoder
+//
+// Parameters:
+// a - binary input   (n bits wide)
+// b - one hot output (m bits wide)
+module Dec(a, b) ;
+  parameter n=2 ;
+  parameter m=4 ;
+
+  input  [n-1:0] a ;
+  output [m-1:0] b ;
+
+  wire [m-1:0] b = 1 << a ;
 endmodule
--- alu_tb.v
+++ alu_tb.v
@@ -1,9 +1,53 @@
 module ALU_tb();
-    reg [15:0] sim_Ain, sim_Bin;
-    reg [1:0] sim_ALUop;
-    reg err;
-    wire [15:0] sim_out;
-    wire sim_Z;
+  reg [15:0] sim_ain, sim_bin;
+  wire [15:0] sim_out;
+  reg [1:0] sim_ALUop;
+  wire sim_Z;
+  reg err;
+
+  ALU DUT(sim_ain, sim_bin, sim_ALUop, sim_out, sim_Z);
+
+  initial begin
+    sim_ain = 16'b0000010101100010; //1378
+    sim_bin = 16'b0101100000100010;//22562
+    err = 1'b0;
+
+//test ADD
+    sim_ALUop = 2'b00;
+    #5;
+    if (sim_out != 16'b0101110110000100) begin
+      $display("output: %b expected: %b, Test1",sim_out, 16'b0101110110000100);
+      err = 1'b1;
+    end
 
 
+//test SUB
+    sim_ALUop = 2'b01;
+    #5;
+    if (sim_out != 16'b1010110101000000) begin
+      $display("output: %b expected: %b ,Test2",sim_out, 16'b1010110101000000);
+      err = 1'b1;
+    end
+
+ //test AND
+    sim_ALUop = 2'b10;
+    #5;
+    if (sim_out != 16'b0000000000100010) begin
+      $display("output: %b expected: %b ,Test3",sim_out, 16'b0000000000100010);
+      err = 1'b1;
+    end
+
+
+//test NOT B
+    sim_ALUop = 2'b11;
+    #5;
+    if (sim_out != 16'b1010011111011101) begin
+      $display("output: %b expected: %b, Test4",sim_out, 16'b1010011111011101);
+      err = 1'b1;
+    end
+
+
+    if (err == 1'b0)
+      $display("yeeted.");
+  end 
 endmodule
