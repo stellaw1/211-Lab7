e5c9ed9cc5758959041df8aab3f0c8eba1f63b21
baknelmacz <baknelmacz@users.noreply.github.com>
2019-10-08 14:21:39 -0700

    fixed shifting functionality

--- shifter.v
+++ shifter.v
@@ -9,13 +9,11 @@ module shifter(in, shift, sout);
     always @(*) begin
         case(shift)
             2'b0: shiftee = in; 
-            2'b01: shiftee << in;
-            2'b10: shiftee >> in;
-            2'b11: begin
-                shiftee[15] = in[15];
-                shiftee >> in;
-            end
+            2'b01: shiftee = 1 << in;
+            2'b10: shiftee = 1 >> in;
+            2'b11: 
+                shiftee = $signed (in)<<<1;
             default: shiftee = 16'bxxxxxxxxxxxxxxxx;
-        end
+        endcase
     end
 endmodule
