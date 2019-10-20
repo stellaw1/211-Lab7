18efb9c4f5fa4d195f58712e185ceafcc19824c0
Stella W <46392676+stellaw1@users.noreply.github.com>
2019-10-20 14:23:50 -0700

    instruction decoder module definition

--- cpu.v
+++ cpu.v
@@ -6,3 +6,37 @@ module cpu(clk, reset, s, load, in, out, N, V, Z, w);
     output N, V, Z, w;
 
 endmodule
+
+//instruction decoder module definition
+module instruction_Decoder(from_instruction_reg, nsel, opcode, op, ALUop, sximm5, sximm8, shift, readnum, writenum);
+    input [15:0] from_instruction_reg; //rename from_instruction_reg input. 
+    input [TBD] nsel; //decide width of nsel from controller FSM
+    output [2:0] opcode, readnum, writenum;
+    output [1:0] op, ALUop, shift;
+    output [15:0] sximm5, sximm8;
+    output  shift, readnum, writenum;
+
+    //to controller FSM
+    assign opcode = from_instruction_reg[15:13];
+    assign op = from_instruction_reg[12:11];
+
+    //to datapath
+    assign ALUop = from_instruction_reg[12:11];
+    assign sximm5 = {11{from_instruction_reg[4]}, from_instruction_reg[3:0]};
+    assign sximm8 = {8{from_instruction_reg[7]}, from_instruction_reg[6:0]};
+    assign shift = from_instruction_reg[4:3];
+    
+    Mux3 #(3) selectNum(from_instruction_reg[10:8], from_instruction_reg[7:5], from_instruction_reg[2:0], nsel, readnum); //check order of inputs
+    assign writenum = readnum;
+endmodule
+
+//3 input k bit MUX, one hot select
+module Mux3(a2, a1, a0, s, b);
+  parameter k = 1;
+  input [k-1:0] a2, a1, a0; //inputs
+  input [2:0] s; //select
+  output [k-1:0] b;
+  wire [k-1:0] b = ({k{s[0]}} & a0) |
+		     ({k{s[1]}} & a1) |
+		     ({k{s[2]}} & a2);
+endmodule
