a6e1e1e87c52d6ac9c7a6444677f5189539f0795
baknelmacz <baknelmacz@users.noreply.github.com>
2019-10-22 18:57:45 -0700

    bug in alu

--- alu.v
+++ alu.v
@@ -58,12 +58,3 @@ endmodule
 // Parameters:
 // a - binary input   (n bits wide)
 // b - one hot output (m bits wide)
-module Dec(a, b) ;
-  parameter n=2 ;
-  parameter m=4 ;
-
-  input  [n-1:0] a ;
-  output [m-1:0] b ;
-
-  wire [m-1:0] b = 1 << a ;
-endmodule
