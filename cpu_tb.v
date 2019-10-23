e91f93372eab0631ebfc78e9e46aecb0e12bf748
baknelmacz <baknelmacz@users.noreply.github.com>
2019-10-22 21:45:34 -0700

    testbenches updated, all working

--- cpu_tb.v
+++ cpu_tb.v
@@ -24,7 +24,7 @@ module cpu_tb();
         begin
             //if the data_out in the instantiated DUT is erraneous, display the actual and expected state, and set err = 1
             if (actual !== expected) begin
-				$display("ERROR: expected: %d, actual: %d at testcase %d", expected, actual, err_num);
+				$display("out error expected: %d, actual: %d error: %d", expected, actual, err_num);
 				err = 1'b1;
 			end
         end
@@ -50,7 +50,7 @@ module cpu_tb();
 		//testcase 1: MOV immediate instruction
 		in = 16'b110_10_000_01100100; //REG[000] = 100
 		load = 1'b1;
-		#40;
+		#50;
 		load = 1'b0;
 		s = 1'b0;
 		#40;
@@ -60,7 +60,7 @@ module cpu_tb();
 		//testcase 2: MOV instruction with shift 
 		in = 16'b110_00_000_001_10_000; //REG[001] = REG[000] / 2 = 50;
 		load = 1'b1;
-		#40;
+		#50;
 		load = 1'b0;
 		s = 1'b0;
 		#40;
@@ -70,20 +70,35 @@ module cpu_tb();
 		//testcase 3: ADD instruction with REG[0] and REG[1] and store in REG[2]
 		in = 16'b101_00_000_010_00_001; //REG[2] = REG[0]+REG[1]  = 150;
 		load = 1'b1;
-		#40;
+		#50;
 		load = 1'b0;
 		s = 1'b0;
 		#40;
 
 		error_check(16'd150,sim_out,3);
 
-		//testcase 4: test reset
-		reset = 1'b1;
-		#10;
+		//testcase 4: AND REG[4] REG[2] REG[0] 
+		in = 16'b100_10_010_100_00_000; //REG[000] = 100
+		load = 1'b1;
+		#50;
+		load = 1'b0;
+		s = 1'b0;
+		#40;
 
-		error_check(5'b11111, DUT.FSM.present_state, 15);
+		error_check(16'd4,sim_out,4);
+		
+		//testcase 5: MVN REG[6] REG[0] calc. ~100
+		in = 16'b101_11_000_011_00_000; //REG[000] = 100
+		load = 1'b1;
+		#50;
+		load = 1'b0;
+		s = 1'b1;
+		#40;
 
-		//print results, whether errors were found
+		error_check(16'd65435,sim_out,5);
+		error_check(1'b1,sim_N,6);
+
+
         if (~err) 
             $display ("ya i knew it would work. EZ");
         else
